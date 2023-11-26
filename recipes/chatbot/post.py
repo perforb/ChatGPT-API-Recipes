02947fbf93be3d1d0da55d5ac1a5aef36a387c11
perforb <dev.perfumed.garden@gmail.com>
2023-11-26 20:18:25 +0900

    fix path

--- recipes/chatbot/post.py
+++ recipes/chatbot/post.py
@@ -1,5 +1,5 @@
-from recipes.chatbot.generator import make_content
-from recipes.chatbot.tweeter import post
+from generator import make_content
+from tweeter import post
 
 if __name__ == '__main__':
     content = make_content()
